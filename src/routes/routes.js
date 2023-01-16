b53e2969268257a2fa924903c7c83e63c54a21eb
DeividGonzalezDev <bienvenidoexperto@gmail.com>
2023-01-16 11:51:20 -0300

    Corrigiendo...

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -3,6 +3,6 @@ import { getSchedules } from "../controllers/controllers.js";
 
 const app = Router();
 
-app.get('/booking', (req, res)=> getSchedules(req, res))
+app.get('/booking', (req, res)=> getSchedules(req, res));
 
 export default app;
